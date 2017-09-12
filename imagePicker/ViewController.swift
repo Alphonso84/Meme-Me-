8150a46a6d827f71cb1ef32e1af61f1f181ffdbe
user <alphonsosensleyii@this-macbook.local>
2017-09-12 13:23:10 -0700

    First commit of Meme-Me

--- imagePicker/ViewController.swift
+++ imagePicker/ViewController.swift
@@ -8,18 +8,89 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    
+    @IBOutlet weak var textFieldTop: UITextField!
+    
+    @IBOutlet weak var textFieldBottom: UITextField!
+    
+    
+    // Dictionary to setup TextField Attributes
+    let memeTextAttributes:[String:Any] = [
+        NSStrokeColorAttributeName: UIColor.black,
+        NSForegroundColorAttributeName: UIColor.white,
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 30)!,
+        NSStrokeWidthAttributeName: -3.0
+    ]
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textFieldTop.text = "TOP"
+        textFieldBottom.text = "BOTTOM"
+        
+        textFieldTop.defaultTextAttributes = memeTextAttributes
+        textFieldBottom.defaultTextAttributes = memeTextAttributes
+        
+        
+       
+        
+        }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        
+    }
+    
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        textFieldTop.textAlignment = NSTextAlignment.center
+        textFieldBottom.textAlignment = NSTextAlignment.center
+        
+    }
+    
+    @IBOutlet weak var selectedImage: UIImageView!
+    
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+
+    @IBAction func pickImage(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+        }
+    
+    @IBAction func imageFromCamera(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+       
+        selectedImage.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+        
+        dismiss(animated: true, completion: nil)
+    
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        dismiss(animated: true, completion: nil)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
